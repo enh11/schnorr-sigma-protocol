ba5f891d722739a674bb11a22c6e45494fdd8e12
enh <enriconh@gmail.com>
2026-03-18 13:21:43 +0100

    End of interactive protocol

--- src/bin/prover.rs
+++ src/bin/prover.rs
@@ -1,22 +1,24 @@
-use k256::{Scalar, elliptic_curve::{PrimeField, PublicKey, sec1::ToEncodedPoint}, pkcs8::{DecodePublicKey}};
-use schnorr::{prover::{Prover}};
-use tokio::{io::{AsyncBufReadExt, AsyncReadExt, AsyncWriteExt, BufReader}, net::TcpStream};
-use std::io;
+use k256::{elliptic_curve::{PublicKey}, pkcs8::DecodePublicKey};
+use schnorr::prover::{Prover};
+use tokio::{io::{self, AsyncBufReadExt, AsyncWriteExt, BufReader}, net::TcpStream};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+        // let s=key_gen(&"sk.pem".to_string(), &"pk.pem".to_string());
+
     let socket = TcpStream::connect("127.0.0.1:8080").await?;
     let (reader, mut writer) = socket.into_split();
     
     let mut reader = BufReader::new(reader);
-// Reading wellcom from the server.
+// Reading welcome from the server.
     let mut server_msg = String::new();
     reader.read_line(&mut server_msg).await?;
     println!("{}", server_msg);
 
 // Insert ID and send it to the server.
     let mut id = String::new();
-    io::stdin().read_line(&mut id)?;
+    let mut stdin_reader = BufReader::new(io::stdin());
+    stdin_reader.read_line(&mut id).await?;
     writer.write_all(id.as_bytes()).await?;
 // Read response from the server
     let mut server_msg = String::new();
@@ -27,16 +29,16 @@ async fn main() -> anyhow::Result<()> {
     let my_pk = PublicKey::read_public_key_der_file("pk.pem").expect("Error in reading Public Key.");
     let mut prover = Prover::new(my_pk);
 // First Round: generate random point.
-    let _ = prover.send_commitment_to_random_value(writer);
+    let _ = prover.send_commitment_to_random_value(&mut writer).await?;
     println!("commit {:?}",prover.commitment.unwrap());
 // Second Round: read challenge from prover and respond.
-let mut buf = [0u8; 32];
-reader.read_exact(&mut buf).await?;
 
-// Safe conversion: reduce modulo curve order
-let c = Scalar::from_repr(buf.into()).unwrap();
-
-    println!("recieved {:?}",c);
+let _ = prover.read_challenge(&mut reader).await?;
+// Third round: send rG + (sk)cG
+let _ = prover.response(&mut writer).await?;
+// Read response of identification 
+    reader.read_line(&mut server_msg).await?;
+    println!("{}", server_msg);
 
     Ok(())
 }
--- src/bin/verifier.rs
+++ src/bin/verifier.rs
@@ -1,23 +1,17 @@
 
 use std::fs;
-use std::io::BufRead;
 use std::path::Path;
 use std::str::FromStr;
 
 use anyhow::Ok;
-use crypto_bigint::subtle::CtOption;
+use schnorr::verifier::Verifier;
 use serde::{Deserialize, Serialize};
 use tokio::io::{AsyncBufReadExt, BufReader};
+use k256::elliptic_curve::sec1::{FromEncodedPoint};
+use k256::{EncodedPoint, PublicKey};
 
-use k256::elliptic_curve::{Field};
-use k256::elliptic_curve::group::prime::PrimeCurveAffine;
-use k256::elliptic_curve::sec1::{FromEncodedPoint, ToEncodedPoint};
-use k256::{EncodedPoint, ProjectivePoint, PublicKey, Scalar, Secp256k1};
-use rand_core::OsRng;
-use crypto_bigint::subtle::Choice;
-use tokio::net::tcp::{OwnedReadHalf, OwnedWriteHalf};
-use tokio::net::{TcpListener, TcpStream};
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::{TcpListener};
+use tokio::io::{AsyncWriteExt};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()>{
@@ -47,60 +41,18 @@ async fn main() -> anyhow::Result<()>{
         let pk: PublicKey = PublicKey::from_encoded_point(&pk).unwrap();
         let mut verifier = Verifier::new(pk);
 //Read commitment from the prover
-        let _ = verifier.read_commitment(&mut reader).await;
-        println!("recieved {:?}",verifier.commitment.unwrap());
+        let _ = verifier.read_commitment(&mut reader).await?;
+        //println!("recieved {:?}",verifier.commitment.unwrap());
 // Send a Challenge
-    let _ = verifier.send_challenge(writer).await;
-    println!("challenge {:?}",verifier.challenge.unwrap());
+    let _ = verifier.send_challenge(&mut writer).await?;
+   // println!("challenge {:?}",verifier.challenge.unwrap());
+    // Read response
+let _ =verifier.verify(&mut reader,&mut writer).await?;
     Ok(())
-}
-pub struct Verifier {
-    pub public_key: PublicKey,   // Y = x·G
-    pub commitment: CtOption<ProjectivePoint>, // R
-    pub challenge: Option<Scalar>,     // c
-}impl Verifier {
-    pub fn new(pk: PublicKey) -> Self {
-        Verifier { public_key: pk, commitment: CtOption::new(ProjectivePoint::IDENTITY, Choice::from(0)), challenge: None }
-    }
-    pub async fn send_challenge(&mut self, mut writer: OwnedWriteHalf) -> anyhow::Result<()> {
-        let c = Scalar::random(&mut OsRng);
-        self.challenge = Some(c);
-        writer.write_all(&c.to_bytes()).await?;
-        Ok(())
-    }
-    pub async fn read_commitment(&mut self, reader: &mut BufReader<OwnedReadHalf>) -> anyhow::Result<()> {
 
-        let mut buf = [0u8; 65];
-        reader.read_exact(&mut buf).await?;
 
-        let encoded = EncodedPoint::from_bytes(&buf)?;
-        let commit = ProjectivePoint::from_encoded_point(&encoded);
-        self.commitment = commit;
-        println!("received {:?}", commit);
-        Ok(())
-    }
-async fn send_random_point(verifier_addr: &str) -> anyhow::Result<()> {
-    // Generate a random u32 value
-    let random_value =Scalar::random(&mut OsRng);
-    println!("Generated: {:?}", random_value);
-    let p = k256::AffinePoint::generator()*random_value;
+}
 
-    // Connect to the verifier
-    let mut stream = TcpStream::connect(verifier_addr).await?;
-    // Send the random value as bytes
-    stream.write_all(&p.to_encoded_point(false).to_bytes()).await?;
-    
-    // Wait for verifier response
-    let mut buf = [0u8; 1024];
-    let n = stream.read(&mut buf).await?;
-    println!("Verifier responded: {}", String::from_utf8_lossy(&buf[..n]));
-
-    Ok(())
-}
-pub fn verfy()->bool {
-    todo!()
-}
-}
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct User {
