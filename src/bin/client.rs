20667710f4a32231d8fbdf118e430a5468eda3ee
enh <enriconh@gmail.com>
2026-04-14 15:14:05 +0200

    improvments in client side

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,70 +1,92 @@
 use anyhow::Ok;
 use k256::{elliptic_curve::{PublicKey}, pkcs8::DecodePublicKey};
 use schnorr::prover::{Prover};
-use tokio::{io::{self, AsyncBufReadExt, AsyncReadExt, AsyncWriteExt, BufReader}, net::TcpStream};
+use tokio::{io::{self, AsyncBufReadExt, AsyncReadExt, AsyncWriteExt, BufReader}, net::{TcpStream, tcp::{OwnedReadHalf, OwnedWriteHalf}}};
+
+pub struct ClientConnection {
+    pub reader: BufReader<OwnedReadHalf>,
+    pub writer: OwnedWriteHalf,
+}
+
+impl ClientConnection {
+    pub async fn read_line(&mut self) -> anyhow::Result<String> {
+        let mut line = String::new();
+        self.reader.read_line(&mut line).await?;
+        Ok(line)
+    }
+
+    pub async fn write_line(&mut self, msg: &str) -> anyhow::Result<()> {
+        self.writer.write_all(msg.as_bytes()).await?;
+        Ok(())
+    }
+
+    pub async fn read_stdin() -> anyhow::Result<String> {
+        let mut input = String::new();
+        let mut stdin = BufReader::new(tokio::io::stdin());
+        stdin.read_line(&mut input).await?;
+        Ok(input)
+    }
+
+    //Main run Loop
+    pub async fn run(&mut self) -> anyhow::Result<()> {
+        // Read welcome message
+        let msg = self.read_line().await?;
+        println!("{}", msg);
+
+        // Get user action
+        let action = Self::read_stdin().await?;
+        self.write_line(&action).await?;
+
+        match action.trim() {
+            "0" => self.run_authentication().await?,
+            "1" => self.run_register().await?,
+            _ => println!("Invalid action"),
+        }
+
+        Ok(())
+    }
+pub async fn run_authentication(&mut self)->anyhow::Result<()> {
+    //  The server ask for the user ID. 
+        let msg = self.read_line().await?;
+        println!("{}", msg);
+
+    // The client send it to the server.
+        let id = Self::read_stdin().await?;
+        self.write_line(&id).await?;
+
+    // Server welcome
+        let msg = self.read_line().await?;
+        println!("{}", msg);
+
+        // Initialize prover
+        let my_pk = PublicKey::read_public_key_der_file("pk.pem")?;
+        let mut prover = Prover::new(my_pk);
+
+        // Schnorr protocol
+        prover.send_commitment_to_random_value(&mut self.writer).await?;
+        prover.read_challenge(&mut self.reader).await?;
+        prover.response(&mut self.writer).await?;
+
+        // Final result
+        let msg = self.read_line().await?;
+        println!("{}", msg);
+
+        Ok(())
+    }
+pub async fn run_register(&mut self)->anyhow::Result<()> {
+    todo!()
+}
+}
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-        // let s=key_gen(&"sk.pem".to_string(), &"pk.pem".to_string());
-
     let socket = TcpStream::connect("127.0.0.1:8080").await?;
-    let (reader, mut writer) = socket.into_split();
-    
-    let mut reader = BufReader::new(reader);
-// Reading welcome from the server.
-    let mut server_msg = String::new();
-    reader.read_line(&mut server_msg).await?;
-    println!("{}", server_msg);
+    let (reader, writer) = socket.into_split();
 
-// Read response from command line and send to the server.
-let mut stdin = BufReader::new(tokio::io::stdin());
-let mut action = String::new();
+    let mut conn = ClientConnection {
+        reader: BufReader::new(reader),
+        writer,
+    };
 
-stdin.read_line(&mut action).await?;
-writer.write_all(action.as_bytes()).await?;
-println!("action is {}",action);
-match action.trim() {
-    "0" => authentication(reader, writer).await?,
-    "1" => {println!("action is register");}
-   
-    _=> {
-        println!("invalid action");
-        
-    }
-}
-
-Ok(())
-}
-pub async fn authentication(mut reader:BufReader<tokio::net::tcp::OwnedReadHalf>, mut writer:tokio::net::tcp::OwnedWriteHalf) -> anyhow::Result<()>{
-  // Insert ID and send it to the server.
-  let mut server_msg = String::new();
-    reader.read_line(&mut server_msg).await?;
-    println!("{}", server_msg);
-    
-    let mut id = String::new();
-    let mut stdin_reader = BufReader::new(io::stdin());
-    stdin_reader.read_line(&mut id).await?;
-    writer.write_all(id.as_bytes()).await?;
-// Read response from the server
-    let mut server_msg = String::new();
-    reader.read_line(&mut server_msg).await?;
-    println!("{}", server_msg);
-
-// Initialization of the prover.
-    let my_pk = PublicKey::read_public_key_der_file("pk.pem").expect("Error in reading Public Key.");
-    let mut prover = Prover::new(my_pk);
-// First Round: generate random point.
-    let _ = prover.send_commitment_to_random_value(&mut writer).await?;
-    //println!("commit {:?}",prover.commitment.unwrap());
-// Second Round: read challenge from prover and respond.
-
-let _ = prover.read_challenge(&mut reader).await?;
-// Third round: send rG + (sk)cG
-let _ = prover.response(&mut writer).await?;
-// Read response of identification 
-server_msg.clear();
-    reader.read_line(&mut server_msg).await?;
-    println!("{}", server_msg);
-
-    Ok(())  
+    conn.run().await
 }
